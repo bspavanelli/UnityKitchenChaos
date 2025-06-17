a6d4ae503db2f794ced26da0357a6ccc109c08c5
Bruno Pavanelli <bspavanelli@gmail.com>
2025-06-17 18:08:11 -0300

    scriptable objects
    Scriptable objects de tomate e pedaço de queijo e clear counter identificando qual ele spawnou no interact

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour
-{
+public class ClearCounter : MonoBehaviour {
+
+    [SerializeField] private KitchenObjectSO _kitchenObjectSO;
+    [SerializeField] private Transform _counterTopPoint;
     public void Interact() {
-        Debug.Log("Interact!");
+        Transform kitchenObjectTransform = Instantiate(_kitchenObjectSO.prefab, _counterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
+
+        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO());
     }
 }
